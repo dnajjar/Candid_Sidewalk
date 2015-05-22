598e5804a0db0e3e13e243ec54bc4fa8372a8446
najjar.dana@gmail.com <najjar.dana@gmail.com>
2015-05-22 14:43:14 -0400

    photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,15 +1,17 @@
 class PhotosController < ApplicationController
   
-  
+  def index
+
+  end 
+
   def create
-    binding.pry
     @photo = Photo.create(photo_params)
   end 
   
   def new
     @photo = Photo.new
   end 
-  
+
   private
   def photo_params
     params.require(:photo).permit(:filepicker_url)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "photos#new"
-  resources :photos, only: [:new, :create]
+  resources :photos, only: [:new, :create, :index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
