8940814bc7a887ae38997e7b5ebed6a4a0aef6d2
najjar.dana@gmail.com <najjar.dana@gmail.com>
2015-05-26 16:25:38 -0400

    no pry

--- app/mailers/incident_mailer.rb
+++ app/mailers/incident_mailer.rb
@@ -3,7 +3,6 @@ class IncidentMailer < ApplicationMailer
     @image = incident.image
     @name = incident.name
     @description = incident.description
-    binding.pry
     attachments.inline['image.jpg'] = File.read(@image.file.file)
     mail(to: "jtaub9503@gmail.com" , subject: 'Welcome to My Awesome Site')
   end
