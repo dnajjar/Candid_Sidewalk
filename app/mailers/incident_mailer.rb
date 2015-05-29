64182268acd5a7f707fdc0b25beb5177a0c51970
najjar.dana@gmail.com <najjar.dana@gmail.com>
2015-05-29 17:34:52 -0400

    stuff

--- app/mailers/incident_mailer.rb
+++ app/mailers/incident_mailer.rb
@@ -6,7 +6,6 @@ class IncidentMailer < ApplicationMailer
     @description = incident.description
     # attachments.inline['image.jpg'] = open(@image).read
     attachments.inline['image.jpg'] = open("https://s3-us-west-2.amazonaws.com/candidsidewalk/#{@image.current_path}").read
-    binding.pry
     mail(to: "najjar.dana@gmail.com" , subject: 'For your consideration')
     # incident.get_email
   end
