ae0c2332480dc750cad0832ada11db82323f645e
najjar.dana@gmail.com <najjar.dana@gmail.com>
2015-05-31 11:57:07 -0400

    sf

--- app/mailers/incident_mailer.rb
+++ app/mailers/incident_mailer.rb
@@ -3,10 +3,11 @@ class IncidentMailer < ApplicationMailer
   def welcome_email(incident)
     @image = incident.image
     @name = incident.name
+    @rep_name = incident.get_name
     @description = incident.description
     # attachments.inline['image.jpg'] = open(@image).read
     attachments.inline['image.jpg'] = open("https://s3-us-west-2.amazonaws.com/candidsidewalk/#{@image.current_path}").read
-    mail(to: "najjar.dana@example.com" , subject: 'For your consideration')
+    mail(to: "najjar.dana@gmail.com" , subject: 'For your consideration')
     # incident.get_email
   end
 end
