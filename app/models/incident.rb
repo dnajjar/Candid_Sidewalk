0b8562b06e571cde9f266039c1706e61f5a1261e
najjar.dana@gmail.com <najjar.dana@gmail.com>
2015-06-08 18:51:41 -0400

    moved button

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -49,12 +49,12 @@ class Incident < ActiveRecord::Base
 
   def location
     @adress = self.reverse_geocode
-    if @address = "5 Beaver Street, New York, NY 10004, USA"
-      @location = "11 Broadway, New York, NY, 10004"
-    else 
+    # if @address = "5 Beaver Street, New York, NY 10004, USA"
+    #   @location = "11 Broadway, New York, NY, 10004"
+    # else 
       @location = @address
-    end 
-    @location
+    # end 
+    # @location
   end 
 
   def zipcode
