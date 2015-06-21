838861a2ad7a9b35c2bf1c4fc19fe2f02edd848f
JulianTaub <jtaub9503@gmail.com>
2015-06-21 17:48:43 -0400

    restyled and fixed app report

--- app/mailers/incident_mailer.rb
+++ app/mailers/incident_mailer.rb
@@ -9,7 +9,7 @@ class IncidentMailer < ApplicationMailer
     @rep_name = incident.get_name
     @location = incident.location
     # attachments.inline['image.jpg'] = open(@image).read
-    attachments.inline['image.jpg'] = open("https://s3-us-west-2.amazonaws.com/candidsidewalk/#{@image.current_path}").read
+    attachments.inline['image.jpg'] = open("#{@image.url}").read
     mail(to: "najjar.dana@gmail.com" , subject: 'For your consideration')
     # incident.get_email
   end
